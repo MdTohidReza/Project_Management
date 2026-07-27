43f81bb79ffbecf94eefc86b3968a639fb45b0ac
Md Tohid Reza <harishalone786@gmail.com>
2026-07-27 13:17:13 +0545

    Frontend Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         <>
             <Toaster />
             <Routes>
-                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Layout />}>
                     <Route index element={<Dashboard />} />
                     <Route path="team" element={<Team />} />
                     <Route path="projects" element={<Projects />} />
