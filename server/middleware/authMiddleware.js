1600228826127ca6ec48063c704119aae28a8b0e
Md Tohid Reza <harishalone786@gmail.com>
2026-08-11 13:51:15 +0545

    Project Completed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,10 @@
 export const protect = async (req, res, next) => {
   try {
-    const {userId} = await req.auth()
-    if(!userId){
-      res.status(401).json({message:"Unauthorized"})
+    const { userId } = await req.auth();
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" }); // ✅ stops here
     }
-    return next()
+    return next();
   } catch (err) {
     console.log(err);
     return res.status(401).json({ message: err.code || err.message });
